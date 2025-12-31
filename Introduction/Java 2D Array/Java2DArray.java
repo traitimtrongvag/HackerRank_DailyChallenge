f6c68db1a31f395453470ea502b60f6225a05569
traitimtrongvag <pan2512811@gmail.com>
2025-12-31 21:59:53 +0700

    Solve Java 2D Array

---
+++ Introduction/Java 2D Array/Java2DArray.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[6][6];
+        
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = bufferedReader.readLine().split(" ");
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        
+        int maxSum = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                        + arr[i+1][j+1]
+                        + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+        
+        System.out.println(maxSum);
+        bufferedReader.close();
+    }
+}
